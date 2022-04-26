ab10f1721b4ce55cd98c3e7facc626406235509b
vaishnavi0701 <90175670+vaishnavi0701@users.noreply.github.com>
2022-04-26 22:37:44 +0530

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,96 @@
+// var X="5"+2+3;
+// console.log(X)
+
+// var X=2+3+"5";
+// console.log(X)
+
+// var x=40+10+"name"+20+40
+// console.log(x)
+
+
+// let x=12;
+// y="name";
+// console.log(x*y)
+
+
+// x=5;
+// console.log(x);
+// // var x;
+
+
+// const x=-3/0;
+// console.log(x)
+
+
+// console.log(typeof(null));
+// a="gayatri"
+// b="shelke"
+// console.log(`my name is ${a}.my surname is ${b}`);
+
+
+
+// let x = 5;
+// let y = 3;
+// console.log("++x",++x)
+// console.log("x++",x++)
+
+// console.log("--x",--x)
+// console.log("x--",x--)
+
+
+
+// // addition
+// console.log('x + y = ', x + y);  // 8
+
+// // subtraction
+// console.log('x - y = ', x - y);  // 2
+
+// // multiplication
+// console.log('x * y = ', x * y);  // 15
+
+// // division
+// console.log('x / y = ', x / y);  // 1.6666666666666667
+
+// // remainder
+// console.log('x % y = ', x % y);   // 2
+
+// increment
+// console.log('++x = ', ++x); // x is now 6
+// console.log('x++ = ', x++); // prints 6 and then increased to 7
+// console.log('x = ', x);     // 7
+
+// // decrement
+// console.log('--x = ', --x); // x is now 6
+// console.log('x-- = ', x--); // prints 6 and then decreased to 5
+// console.log('x = ', x);     // 5
+
+// //exponentiation
+// console.log('x ** y =', x ** y);
+
+
+
+// // equal operator
+// console.log(2 == 2); // true
+// console.log(2 == '2'); // true
+
+// // not equal operator
+// console.log(3 != 2); // true
+// console.log('hello' != 'Hello'); // true
+
+// // strict equal operator
+// console.log(2 === 2); // true
+// console.log(2 === '2'); // false
+
+// // strict not equal operator
+// console.log(2 !== '2'); // true
+// console.log(2 !== 2); // false
+
+
+
+// result = '3' + true; 
+// console.log(result); // "3true"
+
+// var n = 5;
+// for (let i = 1; i <= n; i++) {
+//     console.log(`I love JavaScript.`);
+// }
